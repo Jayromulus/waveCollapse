d6970545d508cd2066afbfb431ad74b17f5e5e0f
Jayromulus <jerbear2@gmail.com>
2023-11-17 20:59:02 -0500

    adding readme

---
+++ readme.md
@@ -0,0 +1,1 @@
+A personal exercise in trying to create a wave collapse function in javscript using DOM. Definitely not performant but meant as a challenge in between other project ideas.
--- scripts/main.js
+++ scripts/main.js
@@ -4,21 +4,80 @@ const grid = [];
 let tracker = 0;
 // import neighborList from './neighbors.json';
 
-const input = document.querySelector('input');
-
+const tileset = ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left'];
 //* Figuring out how to Limit the Neighbors of a Given Tile
 const neighbors = {
-  empty: {
+  'empty': {
     north: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left'],
     east: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left'],
     south: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left'],
     west: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left']
   },
-  center: {
+  'center': {
     north: ['center', 'up-down', 'up-right-down', 'up-down-left', 'right-down', 'right-down-left', 'down-left'],
     east: ['center', 'up-left', 'up-right-left', 'up-down-left', 'right-left', 'right-down-left', 'down-left'],
     south: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left'],
     west: ['center', 'up-left', 'up-right-left', 'up-down-left', 'right-left', 'right-down-left', 'down-left']
+  },
+  'down-left': {
+    north: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left'],
+    east: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left'],
+    south: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left'],
+    west: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left']
+  },
+  'right-down-left': {
+    north: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left'],
+    east: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left'],
+    south: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left'],
+    west: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left']
+  },
+  'right-down': {
+    north: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left'],
+    east: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left'],
+    south: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left'],
+    west: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left']
+  },
+  'right-left': {
+    north: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left'],
+    east: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left'],
+    south: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left'],
+    west: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left']
+  },
+  'up-down-left': {
+    north: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left'],
+    east: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left'],
+    south: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left'],
+    west: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left']
+  },
+  'up-down': {
+    north: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left'],
+    east: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left'],
+    south: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left'],
+    west: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left']
+  },
+  'up-left': {
+    north: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left'],
+    east: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left'],
+    south: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left'],
+    west: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left']
+  },
+  'up-right-down': {
+    north: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left'],
+    east: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left'],
+    south: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left'],
+    west: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left']
+  },
+  'up-right-left': {
+    north: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left'],
+    east: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left'],
+    south: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left'],
+    west: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left']
+  },
+  'up-right': {
+    north: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left'],
+    east: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left'],
+    south: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left'],
+    west: ['center', 'up-right', 'up-down', 'up-left', 'up-right-down', 'up-right-left', 'up-down-left', 'right-down', 'right-left', 'right-down-left', 'down-left']
   }
 }
 //* Initialize the Board/Map to a given number of tiles
@@ -53,6 +112,7 @@ function seedTile() {
   return { element: grid[randomX][randomY], x: randomX, y: randomY }
 }
 
+//* 
 function formatTile(tile) {
   let row, col;
   grid.forEach((r, i) => r.includes(tile) ? row = i : null)
@@ -86,6 +146,22 @@ function limitSwitch(tile, axis, direction) {
   return 500;
 }
 
+function start(tile) {
+  const tileType = tileset[random(tileset.length)];
+
+  // this section will change in order to more efficiently change not only the starter tile but also each generatively created tile
+  tile.element.innerText = 0;
+  tile.element.classList.add(tileType);
+  tile.element.classList.remove('empty');
+
+  //! clump all of this together and make it so that this runs in order of each tile affected, and updated each tile to only contain the allotted items for a given tile. probably instead of having a given tile reference a database maybe each tile should instead inherently contain a list of all allowed tiles and when one is changed near it we just reduce the options until either the last remaining item exists within that updated tile or if there is a standstill then we select randomly one of the tiles with the lowest "evaluation" score and decide randomly what it is that tile should be and then re-evaluate the tiles from there. ideally the "wave" of checks should fizzle out fairly quickly, within one or two tiles, before it has not enough information to update a tile again. this might also only need to affect or check tiles that are adjacent to currently filled tiles if that makes it easier to calculate and reduce load times
+  const validNeighbors = findNeighbors(tile.element);
+
+  Object.keys(validNeighbors).forEach(direction => {
+    updateValidOptions(direction, tile, validNeighbors);
+  })
+}
+
 //! why does this need to be in it's own function, cant i just add all of this into the same function instead of needing to loop over the items 4 times and just run and check each direction in a single function call without needing a switch statement since i will realistically be updateing all 4 at the same time most likely (citation needed)
 function updateValidOptions(direction, starterTile, validNeighbors) {
   let selectedTile, selectedX, selectedY;
@@ -96,11 +172,7 @@ function updateValidOptions(direction, starterTile, validNeighbors) {
           break;
         selectedTile = grid[starterTile.x][selectedY]
         selectedTile.innerText = validNeighbors['north'].length;
-        // console.log(selectedTile)
-        // updateValidOptions('north', formatTile(selectedTile), findNeighbors(selectedTile));
-        // updateValidOptions('east', formatTile(selectedTile), findNeighbors(selectedTile));
-        // updateValidOptions('south', formatTile(selectedTile), findNeighbors(selectedTile));
-        // updateValidOptions('west', formatTile(selectedTile), findNeighbors(selectedTile));
+        updateValidOptions('east', formatTile(selectedTile), findNeighbors(selectedTile))
         break;
       case 'east':
         selectedX = limitSwitch(starterTile, 'x', 'right');
@@ -108,11 +180,6 @@ function updateValidOptions(direction, starterTile, validNeighbors) {
           break;
         selectedTile = grid[selectedX][starterTile.y];
         selectedTile.innerText = validNeighbors['east'].length;
-        //! this is way too heavy for each to recursively run on change. this also means that 
-        // updateValidOptions('north', formatTile(selectedTile), findNeighbors(selectedTile));
-        // updateValidOptions('east', formatTile(selectedTile), findNeighbors(selectedTile));
-        // updateValidOptions('south', formatTile(selectedTile), findNeighbors(selectedTile));
-        // updateValidOptions('west', formatTile(selectedTile), findNeighbors(selectedTile));
         break;
       case 'south':
         selectedY = limitSwitch(starterTile, 'y', 'down');
@@ -120,10 +187,6 @@ function updateValidOptions(direction, starterTile, validNeighbors) {
           break;
         selectedTile = grid[starterTile.x][selectedY];
         selectedTile.innerText = validNeighbors['south'].length;
-        // updateValidOptions('north', formatTile(selectedTile), findNeighbors(selectedTile));
-        // updateValidOptions('east', formatTile(selectedTile), findNeighbors(selectedTile));
-        // updateValidOptions('south', formatTile(selectedTile), findNeighbors(selectedTile));
-        // updateValidOptions('west', formatTile(selectedTile), findNeighbors(selectedTile));
         break;
       case 'west':
         selectedX = limitSwitch(starterTile, 'x', 'left');
@@ -131,10 +194,6 @@ function updateValidOptions(direction, starterTile, validNeighbors) {
           break;
         selectedTile = grid[selectedX][starterTile.y];
         selectedTile.innerText = validNeighbors['west'].length;
-        // updateValidOptions('north', formatTile(selectedTile), findNeighbors(selectedTile));
-        // updateValidOptions('east', formatTile(selectedTile), findNeighbors(selectedTile));
-        // updateValidOptions('south', formatTile(selectedTile), findNeighbors(selectedTile));
-        // updateValidOptions('west', formatTile(selectedTile), findNeighbors(selectedTile));
         break;
     }
 }
@@ -143,36 +202,37 @@ function updateValidOptions(direction, starterTile, validNeighbors) {
 function load(totalTiles, rowLength) {
   //! edit these parameters to effect the total number of tiles and the row length
   init(totalTiles, rowLength);
-  // console.log(grid);
+  console.log(grid);
   // console.log(random(grid[0].length))
   // items are currently being stored in the order of [column][row]
   // can also think of it as reading the 'x' position before 'y'
   const starterTile = seedTile();
-
-  // this section will change in order to more efficiently change not only the starter tile but also each generatively created tile
-  starterTile.element.innerText = 0;
-  starterTile.element.classList.add('center');
-  starterTile.element.classList.remove('empty');
-
-
-  //! clump all of this together and make it so that this runs in order of each tile affected, and updated each tile to only contain the allotted items for a given tile. probably instead of having a given tile reference a database maybe each tile should instead inherently contain a list of all allowed tiles and when one is changed near it we just reduce the options until either the last remaining item exists within that updated tile or if there is a standstill then we select randomly one of the tiles with the lowest "evaluation" score and decide randomly what it is that tile should be and then re-evaluate the tiles from there. ideally the "wave" of checks should fizzle out fairly quickly, within one or two tiles, before it has not enough information to update a tile again. this might also only need to affect or check tiles that are adjacent to currently filled tiles if that makes it easier to calculate and reduce load times
-  const validNeighbors = findNeighbors(starterTile.element);
-  // console.log(validNeighbors);
-
-  Object.keys(validNeighbors).forEach(direction => {
-    updateValidOptions(direction, starterTile, validNeighbors);
-  })
+  start(starterTile);
+  // start();
+  // start();
+  // start();
+  // start();
+  // start();
+  // start();
+  // start();
 }
 
 window.onload = () => load(875, 24);
 
 window.addEventListener('keyup', e => {
   e.preventDefault()
-  // console.log(e)
-  container.innerHTML = '';
-  while(grid[0])
-    grid.pop();
-  load(875, 24);
+  // console.log(e.code)
+  switch(e.code) {
+    case 'KeyR':
+      container.innerHTML = '';
+      while(grid[0])
+        grid.pop();
+      load(875, 24);
+      break;
+    case 'KeyC':
+      console.log('continue the collapse');
+      break;
+  }
 });
 
 /*
